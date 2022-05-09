0313d88ebc23987a3d033f41811e33ac52d46e19
Roman Salnikov <r.salnikov1998@gmail.com>
2022-05-09 21:15:40 +1000

    Implement transform shaker and create prefabs

---
+++ Assets/Scripts/Core/CollisionDetector.cs
@@ -0,0 +1,19 @@
+﻿using Core.Hero;
+using UnityEngine;
+
+namespace Core
+{
+    public class CollisionDetector : MonoBehaviour
+    {
+        [SerializeField] private TransformShaker _shaker;
+        private void OnCollisionEnter(Collision collision)
+        {
+            _shaker.StartShaking(collision);
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            _shaker.StopShaking(other);
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Hero/TransformShaker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Core.Hero
+{
+    public class TransformShaker : MonoBehaviour
+    {
+        [SerializeField] [Range(0,2)] private float _duration;
+        [SerializeField] [Range(0,2)] private float _shakeStrength;
+
+        private bool _collided;
+
+        public void StartShaking(Collision collision)
+        {
+            if(_collided)
+                return;
+            
+            if (collision.gameObject.TryGetComponent(out Movement _))
+            {
+                _collided = true;
+
+                transform.DOShakePosition(_duration, _shakeStrength)
+                    .SetEase(Ease.Linear)
+                    .SetLoops(int.MaxValue)
+                    .SetId(this);
+            }
+        }
+
+        public void StopShaking(Collision other)
+        {
+            if (other.gameObject.TryGetComponent(out Movement _))
+            {
+                _collided = false;
+
+                DOTween.Kill(this);
+            }
+        }
+    }
+}

f07016c3c70b03b11c4d24058ce5a36d2b09055d
Roman Salnikov <r.salnikov1998@gmail.com>
2022-05-09 19:13:56 +1000

    Solve blend shape problem

---
+++ Assets/Scripts/Core/Hero/BlendShape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core.Hero
+{
+    public class BlendShape : MonoBehaviour
+    {
+        [SerializeField] private SkinnedMeshRenderer _skinnedMeshRenderer;
+        [SerializeField] [Range(0, 100)] private float _value;
+
+        private void LateUpdate()
+        {
+            _skinnedMeshRenderer.SetBlendShapeWeight(0, _value);
+        }
+    }
+}
